d51f37aa880fd30e930da5daa33cbac5daa01e6a
Artem Chayko <artem.chayko@knu.ua>
2024-02-14 17:47:04 +0200

    added gcd

--- utils.py
+++ utils.py
@@ -3,3 +3,13 @@ def fact(n):
 	for i in range(1, n+1):
 		m*=i
 	return m
+def gcd(a, b):
+    if a == 0:
+        return b
+    if b == 0:
+        return a
+    if a > b:
+        return gcd(a % b, b)
+    else:
+        return gcd(a, b % a)
+
